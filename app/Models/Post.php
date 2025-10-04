a502d3d9cbcb65f07e055b85b0f15756da499168
iqmalakur <iakbarkurnia@gmail.com>
2025-10-04 21:10:25 +0700

    feat: create models and migrations

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'title',
+        'content',
+        'slug',
+        'published_at',
+    ];
+}

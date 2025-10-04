f4e76b5630b8275ee3fc4da60d35e2cf237d95aa
iqmalakur <iakbarkurnia@gmail.com>
2025-10-04 22:11:13 +0700

    feat: add timestamp seed for user admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,13 +14,17 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $now = Carbon::now();
+
         DB::table('users')->insert([
             [
                 "name" => "Admin",
                 "email" => "admin@akurblog.com",
-                "email_verified_at" => Carbon::now(),
+                "email_verified_at" => $now,
                 "password" => bcrypt("admin1234"),
                 "role" => "admin",
+                "created_at" => $now,
+                "updated_at" => $now,
             ],
         ]);
     }

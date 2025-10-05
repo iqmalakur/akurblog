edb95deeefa251a84a118e3049469f1619c96be1
iqmalakur <iakbarkurnia@gmail.com>
2025-10-05 21:11:29 +0700

    feat: handle route index

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.index')
+
+@section('content')
+<div class="py-8 px-20">
+    <h1 class="text-3xl text-center font-bold mb-5">Blog Terbaru</h1>
+
+    @foreach ($posts as $post)
+        <div class="w-full p-6 my-2 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+            <a href="{{ route('posts.show', $post->id) }}">
+                <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $post->title }}</h5>
+            </a>
+            <h6 class="mb-2 font-normal text-gray-600 dark:text-gray-300">Oleh {{ $post->user->name }} - {{ $post->published_at->format('d-m-Y H:i') }}</h6>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 truncate">{{ $post->content }}</p>
+            <a href="{{ route('posts.show', $post->id) }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Selengkapnya
+                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+        </div>
+    @endforeach
+</div>
+@endsection

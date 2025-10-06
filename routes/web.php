5de88ad3a1a049a7d9cb3e90a0d0cb33ed6442f1
iqmalakur <iakbarkurnia@gmail.com>
2025-10-06 09:49:31 +0700

    feat: handle home view temporarly

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['title' => 'Home']);
 });
 
 Route::resource('posts', PostController::class);

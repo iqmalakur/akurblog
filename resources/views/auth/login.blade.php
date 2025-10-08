4c57e444854471cc6d79c22c6be7ab93bd11c001
iqmalakur <iakbarkurnia@gmail.com>
2025-10-08 21:04:51 +0700

    feat: add cursor pointer to login button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,7 @@
           </label>
         </div>
         <button type="submit"
-          class="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300">Login</button>
+          class="cursor-pointer rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300">Login</button>
       </form>
 
     </div>

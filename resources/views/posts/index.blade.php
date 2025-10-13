92b0954de40203ccfaea5b0492700d14aa9c7641
iqmalakur <iakbarkurnia@gmail.com>
2025-10-13 17:00:50 +0700

    feat: add category information to posts list and post detail

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,14 +9,15 @@
         <a href="{{ route('posts.show', $post->id) }}">
           <h5 class="text-2xl font-bold tracking-tight text-gray-900">{{ $post->title }}</h5>
         </a>
+        <h6 class="mb-1 font-bold text-gray-600">{{ $post->category->name }}</h6>
         <h6 class="mb-2 font-normal text-gray-600">Oleh {{ $post->user->name }} -
           {{ $post->published_at->format('d-m-Y H:i') }}</h6>
         <p class="mb-3 truncate font-normal text-gray-700">{{ $post->content }}</p>
         <a href="{{ route('posts.show', $post->id) }}"
           class="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300">
           Selengkapnya
-          <svg class="ms-2 h-3.5 w-3.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-            viewBox="0 0 14 10">
+          <svg class="ms-2 h-3.5 w-3.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+            fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="M1 5h12m0 0L9 1m4 4L9 9" />
           </svg>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -16,6 +16,7 @@
         </div>
       @endif
       <h1 class="mb-5 text-3xl font-bold md:text-4xl lg:text-5xl">{{ $post->title }}</h1>
+      <h5 class="mb-1 text-base font-bold text-gray-400 md:text-lg">{{ $post->category->name }}</h5>
       <h5 class="mb-8 text-base text-gray-400 md:text-lg">{{ $post->user->name }} ·
         {{ $post->published_at->format('d-m-Y H:i') }}</h5>
       <p class="text-base md:text-lg">{{ $post->content }}</p>
